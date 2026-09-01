7a4bf0c58ee635768ffa0890e4df0403cc592943
Temperz87 <temperz871@gmail.com>
2026-09-01 11:46:41 -0400

    Initial Plugin

---
+++ SUPERHOT_MCD_Mod/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace SUPERHOT_MCD_Mod;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        harmony.PatchAll();
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
